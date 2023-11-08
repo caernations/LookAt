ad0bb5629fc0f4220faa2e8b38c36052f6ee5157
stargeaze <yasminfsalma@gmail.com>
2023-11-08 14:08:29 +0700

    style frontend

--- frontend/src/app/components/HowToUse.jsx
+++ frontend/src/app/components/HowToUse.jsx
@@ -5,7 +5,7 @@ const HowToUse = () => {
     <section id="how-to-use">
       <div>
         <h1 className="mt-20 text-center text-xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-[#181818] via-[#6e6e6e] to-[#a4a4a4] font-black">
-          How To Use
+          HOW TO USE
         </h1>
       </div>
     </section>
