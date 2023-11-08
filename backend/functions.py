15ac3f997fe285afb57e2f80e91dd81b32cde45d
satriadhikara <satriadhikara@gmail.com>
2023-11-08 18:28:46 +0700

    set hsv datasets

--- backend/functions.py
+++ backend/functions.py
@@ -1,8 +1,36 @@
 from PIL import Image
+import os
 
 
 def main():
-    imagePATH = "./image/image.jpg"  # Path to image
+    imagePATH = "./image/image.jpg"  # Path to image (directly to image.jpg)
+    datasetPATH = "./dataset"  # Path to dataset folder
+    datasetFiles = os.listdir(datasetPATH)
+
+    listDatasets = [None for _ in range(len(datasetFiles))]
+
+    for i in range(len(datasetFiles)):
+        listDatasets[i] = Image.open(os.path.join(datasetPATH, datasetFiles[i]))
+
+        if listDatasets[i].mode != "RGB":
+            listDatasets[i] = listDatasets[i].convert("RGB")
+
+        width, height = listDatasets[i].size
+
+        listDatasets[i] = list(
+            Image.open(os.path.join(datasetPATH, datasetFiles[i])).getdata()
+        )
+
+        listDatasets[i] = [
+            listDatasets[i][j * width : (j + 1) * width] for j in range(height)
+        ]
+
+        # Convert RGB to HSV
+        for j in range(height):
+            for k in range(width):
+                r, g, b = listDatasets[i][j][k]
+                h, s, v = convertRGBToHSV(r, g, b)
+                listDatasets[i][j][k] = (h, s, v)
 
     image = Image.open(imagePATH)  # Image variable
 
@@ -18,6 +46,26 @@ def main():
             r, g, b = pixelMatrix[i][j]
             h, s, v = convertRGBToHSV(r, g, b)
             pixelMatrix[i][j] = (h, s, v)
+
+    # Pokoknya kalo mau akses foto-foto yang di dataset ada di listDatasets
+    # Tinggal ambil index nya
+    # Kalo mau akses foto image ada di pixelMatrix
+    # fyi (semua foto sudah diubah ke HSV)
+
+
+def compareImage(pixelMatriksImage, pixelMatriksDataset):
+    if (
+        len(pixelMatriksImage)
+        != len(pixelMatriksDataset) | len(pixelMatriksImage[0])
+        != len(pixelMatriksDataset[0])
+    ):
+        return False
+    else:
+        for i in range(len(pixelMatriksImage)):
+            for j in range(len(pixelMatriksImage[0])):
+                if pixelMatriksImage[i][j] != pixelMatriksDataset[i][j]:
+                    return False
+        return True
 
 
 def convertRGBToHSV(r, g, b):
