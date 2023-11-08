92f36cd66d2a93eb28ca99cb758dbaf2b6b5ca51
stargeaze <yasminfsalma@gmail.com>
2023-11-08 14:08:36 +0700

    style footer

--- frontend/src/app/components/Footer.jsx
+++ frontend/src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-screen mx-auto bottom-0 left-0 right-0 bg-[#0c0c0c] bg-opacity-100">
+    <footer className="h-28 w-screen mx-auto bottom-0 left-0 right-0 bg-[#0c0c0c] bg-opacity-100">
       <div className="container mx-auto flex flex-col items-center justify-center md:flex-row">
         <p className="text-sm text-center md:text-left">
           &copy; {new Date().getFullYear()} Kelompok 25. All rights reserved.
