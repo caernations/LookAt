96b56a3e361c6f8b8a82f7f3bd0315c7875023b4
fnathas <farrel.saskoro@gmail.com>
2023-11-09 11:15:10 +0700

    add function for texture CBIR

--- backend/texture.py
+++ backend/texture.py
@@ -55,6 +55,40 @@ def GLCM(image):
     glcm = normaliseSymmetricMatrix(frameworkMatrix)
     return glcm
 
+def metric(image):
+    contrast = 0
+    homogeneity = 0
+    entropy = 0
+    glcm = GLCM(image)
+    for i in range(256):
+        for j in range(255):
+            contrast += (glcm[i][j] * ((i-j) ** 2))
+            homogeneity += (glcm[i][j]) / (1 + ((i-j) ** 2))
+            entropy += ((glcm[i][j]) * math.log10(glcm[i][j]))
+    return contrast, homogeneity, -entropy
+
+def cosineSimilarityTexture(image1, image2):
+    c1, h1, e1 = metric(image1)
+    c2, h2, e2 = metric(image2)
+
+    dotProductC = sum(a * b for a, b in zip(c1, c2))
+    magnitudeC1 = math.sqrt(sum(a**2 for a in c1))
+    magnitudeC2 = math.sqrt(sum(b**2 for b in c2))
+    resultC = dotProductC / (magnitudeC1 * magnitudeC2)
+
+    dotProductH = sum(a * b for a, b in zip(h1, h2))
+    magnitudeH1 = math.sqrt(sum(a**2 for a in h1))
+    magnitudeH2 = math.sqrt(sum(b**2 for b in h2))
+    resultH = dotProductH / (magnitudeH1 * magnitudeH2)
+
+    dotProductE = sum(a * b for a, b in zip(e1, e2))
+    magnitudeE1 = math.sqrt(sum(a**2 for a in e1))
+    magnitudeE2 = math.sqrt(sum(b**2 for b in e2))
+    resultE= dotProductE / (magnitudeE1 * magnitudeE2)
+
+    overallResult = (resultC + resultH + resultE) / 3
+
+    return overallResult
 
 def transposeMatrix(matrix):
     return [list(i) for i in zip(*matrix)]
