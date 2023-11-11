602e890cb3bb68752c93b2b34798b3dd2c5d90a2
stargeaze <yasminfsalma@gmail.com>
2023-11-11 21:57:09 +0700

    feat camera

--- frontend/src/app/components/ImageInput.jsx
+++ frontend/src/app/components/ImageInput.jsx
@@ -1,14 +1,19 @@
 "use client";
-import React, { useRef, useState } from "react";
-import { PhotoIcon } from "@heroicons/react/24/solid";
-import { CameraIcon } from "@heroicons/react/24/solid";
-import { FolderOpenIcon } from "@heroicons/react/24/solid";
+import React, { useRef, useState, useEffect } from "react";
+import {
+  PhotoIcon,
+  CameraIcon,
+  XCircleIcon,
+  FolderOpenIcon,
+} from "@heroicons/react/24/solid";
 
 const ImageInput = () => {
   const fileInputRef = useRef(null);
+  const videoRef = useRef(null); // Create a ref for the video element
   const [selectedImage, setSelectedImage] = useState(null);
   const [isDragging, setIsDragging] = useState(false);
   const [toggleState, setToggleState] = useState(false);
+  const [showCamera, setShowCamera] = useState(false);
 
   const handleImageUpload = () => {
     fileInputRef.current.click();
@@ -53,8 +58,50 @@ const ImageInput = () => {
     setToggleState(!toggleState);
   };
 
+  // Function to open the camera
+  const openCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+    } catch (err) {
+      console.error("Error accessing the camera", err);
+      alert(`Error accessing the camera: ${err.message}`);
+    }
+  };
+
+  // Effect that runs when showCamera state changes
+  useEffect(() => {
+    if (showCamera) {
+      openCamera();
+    }
+  }, [showCamera]);
+
+  // Function to handle the camera button click
+  const handleOpenCamera = () => {
+    setShowCamera(true);
+  };
+
   return (
     <section>
+      {showCamera && (
+        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center z-10">
+          <div className="relative p-4 w-full max-w-md">
+            <video
+              ref={videoRef}
+              autoPlay
+              playsInline
+              className="rounded-lg"
+              style={{ transform: "scaleX(-1)" }} // CSS for mirroring the video
+            ></video>
+            <button
+              onClick={() => setShowCamera(false)}
+              className="absolute top-0 right-0 m-4"
+            >
+              <XCircleIcon className="h-8 w-8 text-white" />
+            </button>
+          </div>
+        </div>
+      )}
       <div
         className={`mx-auto bg-opacity-50 mt-16 grid grid-cols-1 md:grid-cols-5 gap-5 bg-[#373737] p-4 rounded-3xl w-full md:w-[900px] border-8 border-opacity-10 border-[#373737] ${
           isDragging ? "border-gray-600" : ""
@@ -85,13 +132,12 @@ const ImageInput = () => {
         <div className="md:col-span-2 bg-[#EEEEEE] h-[300px] md:h-[300px] w-full flex flex-col items-center justify-center">
           <button
             className="h-9 flex items-center justify-center space-x-2 bg-[#181818] bg-opacity-30 text-white px-4 py-2 rounded-full shadow-lg hover:scale-105 hover:bg-opacity-50 transition-colors duration-200"
-            onClick={handleImageUpload}
+            onClick={handleOpenCamera}
           >
             <CameraIcon className="h-5 w-5 text-white" />
             <span className="font-bold">| Open Camera</span>
           </button>
 
-          
           <button
             className="h-9 mt-2 flex items-center justify-center space-x-2 bg-[#181818] bg-opacity-30 text-white px-4 py-2 rounded-full shadow-lg hover:scale-105 hover:bg-opacity-50 transition-colors duration-200"
             onClick={handleImageUpload}
