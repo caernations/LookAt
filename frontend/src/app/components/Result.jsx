9689a46f0074b3ecb8a385f1a61c37544d57a8cb
stargeaze <yasminfsalma@gmail.com>
2023-11-13 13:34:23 +0700

    update result

--- frontend/src/app/components/Result.jsx
+++ frontend/src/app/components/Result.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from "react";
 import { Document, Page, Image, pdf } from "@react-pdf/renderer";
-import { saveAs } from 'file-saver'; // Make sure this is installed
+import { saveAs } from 'file-saver'; 
 import { FolderArrowDownIcon } from "@heroicons/react/24/solid";
 
 const imageResults = [
@@ -29,10 +29,8 @@ const imageResults = [
     imagePath: "../../images/image5.png",
     alt: "Result",
   },
-  // ... other images
 ];
 
-// PDF Document Component
 const MyDocument = ({ images }) => (
   <Document>
     {images.map((imagePath, index) => (
@@ -43,14 +41,14 @@ const MyDocument = ({ images }) => (
   </Document>
 );
 
-// Result Component
-const Result = ({ searchInitiated }) => { // Receive the prop here
+
+const Result = ({ searchInitiated }) => { 
   const imagePaths = imageResults.map(data => data.imagePath);
 
-  // Generate PDF and trigger download
+
   const downloadPDF = async () => {
     const doc = <MyDocument images={imagePaths} />;
-    const asPdf = pdf(); // Initialize a new PDF instance
+    const asPdf = pdf();
     asPdf.updateContainer(doc);
     const blob = await asPdf.toBlob();
     saveAs(blob, 'download.pdf');
@@ -66,7 +64,6 @@ const Result = ({ searchInitiated }) => { // Receive the prop here
             <img key={index} src={item.imagePath} alt={item.alt} className="aspect-square h-48" />
           ))
         ) : (
-          // Render placeholder divs if search has not been initiated
           <div className="bg-blue-100 aspect-square h-48"></div>
         )}
         {searchInitiated ? (
@@ -74,7 +71,6 @@ const Result = ({ searchInitiated }) => { // Receive the prop here
             <img key={index} src={item.imagePath} alt={item.alt} className="aspect-square h-48" />
           ))
         ) : (
-          // Render placeholder divs if search has not been initiated
           <div className="bg-blue-100 aspect-square h-48"></div>
         )}
         {searchInitiated ? (
@@ -82,7 +78,6 @@ const Result = ({ searchInitiated }) => { // Receive the prop here
             <img key={index} src={item.imagePath} alt={item.alt} className="aspect-square h-48" />
           ))
         ) : (
-          // Render placeholder divs if search has not been initiated
           <div className="bg-blue-100 aspect-square h-48"></div>
         )}
         {searchInitiated ? (
@@ -90,7 +85,6 @@ const Result = ({ searchInitiated }) => { // Receive the prop here
             <img key={index} src={item.imagePath} alt={item.alt} className="aspect-square h-48" />
           ))
         ) : (
-          // Render placeholder divs if search has not been initiated
           <div className="bg-blue-100 aspect-square h-48"></div>
         )}
         {searchInitiated ? (
@@ -98,7 +92,6 @@ const Result = ({ searchInitiated }) => { // Receive the prop here
             <img key={index} src={item.imagePath} alt={item.alt} className="aspect-square h-48" />
           ))
         ) : (
-          // Render placeholder divs if search has not been initiated
           <div className="bg-blue-100 aspect-square h-48"></div>
         )}
         {searchInitiated ? (
@@ -106,7 +99,6 @@ const Result = ({ searchInitiated }) => { // Receive the prop here
             <img key={index} src={item.imagePath} alt={item.alt} className="aspect-square h-48" />
           ))
         ) : (
-          // Render placeholder divs if search has not been initiated
           <div className="bg-blue-100 aspect-square h-48"></div>
         )}
       </div>
