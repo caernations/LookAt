c6e40a107cd0e8006f2b4f0943edee103c0e65e4
satriadhikara <satriadhikara@gmail.com>
2023-11-15 19:03:40 +0700

    refactor: setting up the backend (final)

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from color import color
 from texture import texture
+import uvicorn
 
 app = FastAPI()
+
+# Configure CORS
+origins = [
+    "http://localhost:3000",  # Allow requests from this origin
+    # Add more origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/upload")
@@ -18,77 +35,10 @@ async def upload_files(
         return await color(dataset, image)
 
 
-# @app.get("/")
-# def main():
-#     return HTMLResponse(
-#         """<html lang="en">
-#   <head>
-#     <meta charset="UTF-8" />
-#     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-#     <title>Tubes Bentar</title>
-#     <script>
-#       window.onload = function () {
-#         document.getElementById("form1").onsubmit = function (event) {
-#           event.preventDefault();
-
-#           var formData = new FormData(this);
-#           var startTime = performance.now();
-
-#           fetch("/upload", {
-#             method: "POST",
-#             body: formData,
-#           })
-#             .then((response) => response.json())
-#             .then((data) => {
-#               var endTime = performance.now();
-#               var timeTaken = endTime - startTime;
-
-#               var imagesContainer = document.getElementById("images");
-#               imagesContainer.innerHTML = ""; // Clear the container
-
-#               // Display the time taken
-#               var timeElement = document.createElement("p");
-#               timeElement.textContent =
-#                 "Time taken: " + Math.round(timeTaken) + " ms";
-#               imagesContainer.appendChild(timeElement);
-
-#               data.forEach((imageData) => {
-#                 var img = document.createElement("img");
-#                 img.src = `data:image/jpeg;base64,${imageData.base64imagedata}`;
-#                 imagesContainer.appendChild(img);
-
-#                 var similarityElement = document.createElement("p");
-#                 similarityElement.textContent =
-#                   "Similarity: " +
-#                   imageData.similaritypercentage.toFixed(5) +
-#                   "%";
-#                 imagesContainer.appendChild(similarityElement);
-#               });
-#             });
-#         };
-#       };
-#     </script>
-#   </head>
-#   <body>
-#     <form id="form1" enctype="multipart/form-data">
-#       <label for="dataset">Select dataset:</label>
-#       <input type="file" name="dataset" multiple />
-
-#       <label for="image">Select root image:</label>
-#       <input type="file" name="image" />
-
-#       <label for="color">Color</label>
-#       <input type="radio" name="choice" id="color" value="color" />
-
-#       <label for="texture">Texture</label>
-#       <input type="radio" name="choice" id="texture" value="texture" />
-
-#       <input type="submit" value="Submit" />
-#     </form>
-#     <div id="images"></div>
-#   </body>
-# </html>
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return FileResponse("index.html")
 
 
-# """
-#     )
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
