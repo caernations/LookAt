1a71ab511115fbd0056ad73b2199a2d31bb491b4
stargeaze <yasminfsalma@gmail.com>
2023-11-18 18:52:04 +0700

    update

--- frontend/src/app/components/HowToUse.jsx
+++ frontend/src/app/components/HowToUse.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HowToUse = () => {
   return (
     <section id="how-to-use" className="w-full">
-      <svg
+      {/* <svg
         className="w-full"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
@@ -13,9 +13,9 @@ const HowToUse = () => {
           fill-opacity="0.4"
           d="M0,192L60,165.3C120,139,240,85,360,80C480,75,600,117,720,144C840,171,960,181,1080,176C1200,171,1320,149,1380,138.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
         ></path>
-      </svg>
+      </svg> */}
       <div>
-        <h1 className="mt-80 mb-12 text-center text-xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-[#181818] via-[#6e6e6e] to-[#a4a4a4] font-black">
+        <h1 className="mt-40 mb-12 text-center text-xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-[#181818] via-[#6e6e6e] to-[#a4a4a4] font-black">
           HOW TO USE
         </h1>
         <div className="relative flex justify-center items-center">
