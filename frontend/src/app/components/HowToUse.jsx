90e4df0cf5a4328740c748f2ec22440c4057cd8c
stargeaze <yasminfsalma@gmail.com>
2023-11-19 17:07:14 +0700

    update how to use

--- frontend/src/app/components/HowToUse.jsx
+++ frontend/src/app/components/HowToUse.jsx
@@ -1,49 +1,68 @@
 import React from "react";
 
+const steps = [
+  {
+    id: 1,
+    title: "DATASET SUBMISSION",
+    description: "Submit your image set once for multiple CBIR searches. Changing the dataset requires a page refresh.",
+  },
+  {
+    id: 2,
+    title: "STEP 2",
+    description: "Upload a query image or use the website's camera for continuous snapshots.",
+  },
+  {
+    id: 3,
+    title: "STEP 3",
+    description: "Adjust parameters for search by color or texture",
+  },
+  {
+    id: 4,
+    title: "STEP 4",
+    description: "Click the 'Search' button to find matching images",
+  },
+  {
+    id: 5,
+    title: "STEP 5",
+    description: "View a curated selection of images along with the percentage of similarity that exceeds 60%",
+  },
+];
+
 const HowToUse = () => {
   return (
-    <section id="how-to-use" className="w-full">
-      {/* <svg
-        className="w-full"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          fill="#343434"
-          fill-opacity="0.4"
-          d="M0,192L60,165.3C120,139,240,85,360,80C480,75,600,117,720,144C840,171,960,181,1080,176C1200,171,1320,149,1380,138.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-        ></path>
-      </svg> */}
-      <div>
-        <h1 className="mt-40 mb-12 text-center text-xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-[#181818] via-[#6e6e6e] to-[#a4a4a4] font-black">
+    <section
+      id="howtouse"
+      className="mb-40 mx-auto w-full h-[1900px] md:h-[1300px] bg-primary flex flex-col items-center"
+    >
+      <div className="mb-40">
+        <p className="text-center text-xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-[#181818] via-[#6e6e6e] to-[#a4a4a4] font-black">
           HOW TO USE
-        </h1>
-        <div className="relative flex justify-center items-center">
-          {/* Vertical line */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-2 bg-black"></div>
-
-          <div className="flex flex-col space-y-4 mr-2">
-            <div className="bg-[#343434] bg-opacity-30 h-24 w-80 text-white py-2 px-4 rounded-full mb-8">
-              STEP 1
-            </div>
-            <div className="bg-[#343434] bg-opacity-30 h-24 w-80 text-white py-2 px-4 rounded-full mb-8">
-              STEP 3
-            </div>
-            <div className="bg-[#343434] bg-opacity-30 h-24 w-80 text-white py-2 px-4 rounded-full mb-8">
-              STEP 5
-            </div>
-          </div>
-
-          <div className="flex flex-col space-y-4 ml-2">
-            <div className="bg-[#343434] bg-opacity-30 h-24 w-80 text-white py-2 px-4 rounded-full mb-8">
-              STEP 2
-            </div>
-            <div className="bg-[#343434] bg-opacity-30 h-24 w-80 text-white py-2 px-4 rounded-full">
-              STEP 4
-            </div>
-          </div>
-        </div>
+        </p>
       </div>
+      <div
+        className="mt-[5rem] h-[1800px] md:h-[1300px] absolute left-50% w-[5px] bg-white rounded z-5"
+        // data-aos="zoom-in"
+      />
+      {steps.map((item, index) => {
+        return (
+          <div
+            className={`bg-[#121212] w-[350px] h-[200px] rounded-3xl shadow-xl transition-transform hover:scale-105 hover:shadow-2xl p-5 ${
+              index % 2 === 0 ? "ml-[400px] mb-8" : "mr-[400px] mb-8"
+            }
+        `}
+            key={index}
+            // data-aos="zoom-in"
+          >
+            <div className="w-[97.5%] h-[97%] rounded bg-primary text-[#ADB7BE] text-center p-3 z-10 flex flex-col justify-center md:justify-none space-y-3">
+              <h2 className="font-bold text-xl">{item.title}</h2>
+              <hr className="border-white" />
+              <p className="font-medium font-gradient">
+                {item.description}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
